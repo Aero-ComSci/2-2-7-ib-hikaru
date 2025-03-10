e64e171f89085057f9071cde6e5bc3850dd2fd08
egballl <148280195+egballl@users.noreply.github.com>
2025-03-10 14:37:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -41,3 +41,7 @@ url_entry.pack(side=tk.LEFT)
 
 frame = tk.Frame(root,  bg="black") # change frame color
 frame.pack()
+
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()
